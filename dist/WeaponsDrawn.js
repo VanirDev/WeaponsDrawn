ca447719208915b5f693de342f6bcc17873e8de3
L Jones <36479515+VanirDev@users.noreply.github.com>
2021-01-19 01:39:30 +0000

    Added initial changes to token settings ui, currently lacking save/exit events.

--- dist/WeaponsDrawn.js
+++ dist/WeaponsDrawn.js
@@ -9,11 +9,15 @@ import { preloadTemplates } from './module/preloadTemplates.js';
 import { DND5E } from "../../systems/dnd5e/module/config.js";
 
 /* ------------------------------------ */
+/* CONSTANTS							*/
+/* ------------------------------------ */
+const defaultIcon = 'icons/svg/mystery-man.svg';
+
+/* ------------------------------------ */
 /* Initialize module					*/
 /* ------------------------------------ */
 Hooks.once('init', async function () {
 	console.log('Weapons Drawn | Initializing WeaponsDrawn');
-
 	// Assign custom classes and constants here
 
 	// Register custom module settings
@@ -38,3 +42,55 @@ Hooks.once('setup', function () {
 Hooks.once('ready', function () {
 	// Do anything once the module is ready
 });
+
+Hooks.on('renderTokenConfig', onRenderTokenConfig);
+
+function onRenderTokenConfig (tokenConfig, html) {
+	const tokenImageDiv = $("input.image").parent().parent();
+	console.log(tokenImageDiv);
+	
+	let headerHTML = 
+		`<div class="form-group">
+			<label>Token Icons</label>
+		</div>`;
+
+	let defaultIconHTML = 
+		`<div class="form-group">
+			<label>Out of Combat Token Image:</label>
+			<div class="form-fields">
+				<button type="button" class="file-picker" data-type="imagevideo" data-target="imgIdle" title="Browse Files" tabindex="-1">
+					<i class="fas fa-file-import fa-fw">
+					</i>
+				</button>
+				<input id="WDIdleTokenPathBox" class="image" type="text" name="imgIdle" placeholder="path/image.png" value="">
+			</div>
+		</div>`;
+
+	let combatIconHTML = 
+		`<div class="form-group">
+			<label>In Combat Token Image:</label>
+			<div class="form-fields">
+				<button type="button" class="file-picker" data-type="imagevideo" data-target="imgCombat" title="Browse Files" tabindex="-1">
+					<i class="fas fa-file-import fa-fw">
+					</i>
+				</button>
+				<input id="WDCombatTokenPathBox" class="image" type="text" name="imgCombat" placeholder="path/image.png" value="">
+			</div>
+		</div>`;
+
+	tokenImageDiv.before(headerHTML);
+	tokenImageDiv.before(defaultIconHTML);
+	tokenImageDiv.before(combatIconHTML);
+	tokenImageDiv.hide();
+
+	tokenImageDiv.prev().prev().click(async (ev) => {
+		await new FilePicker({ callback: (path) => {
+			$("#WDIdleTokenPathBox").val(path);
+		}}).render(true);
+	});
+	tokenImageDiv.prev().click(async (ev) => {
+		await new FilePicker({ callback: (path) => {
+			$("#WDCombatTokenPathBox").val(path);
+		}}).render(true);
+	});
+}
