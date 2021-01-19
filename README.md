7b3fcf74f15ee3096a58ee25a5db8eb8dc53baa8
L Jones <36479515+VanirDev@users.noreply.github.com>
2021-01-19 17:57:25 +0000

    Initial Release Content

---
+++ README.md
@@ -0,0 +1,42 @@
+![](https://img.shields.io/badge/Foundry-v0.7.9-blue) ![](https://img.shields.io/badge/D&D5e-v1.2.2-blue)
+
+# Weapons Drawn
+
+A visual utility plugin, which allows characters to have a secondary token image which is automatically swapped to when in combat.
+
+## Update Changelogs
+
+<details>
+  <summary>Click to Expand</summary>
+  
+  ### Release 0.1
+  
+  * Initial release, allowing for per-actor configuration of tokens, with a per-actor toggle for WD tokens.
+</details>
+
+## Features
+
+### Dynamic Token Switch
+
+![]()
+
+Tokens configured with this module will automatically swap their token when entering/leaving a combat state.
+
+### Settings UI
+
+![]()
+
+Every actor now has a toggle for WD token images. When enabled, the standard token path input is replaced by two inputs, which are used for out of combat and in combat states.
+
+##### As a disclaimer, this module currently works on a per-actor basis, regardless of whether tokens are linked to their actor. All tokens that derive from the same actor will be modified according to the WD settings, so it suggested that this primarily be used for player characters or creatures that don't need individually unique tokens if they are copied from the same base actor. This will likely be improved in a future version to remove this limitation.
+
+## Installation
+
+1. Open the "Add-On Modules" tab inside the FoundryVTT setup section.
+2. Click "Install Module" and paste this link into the "Manifest URL" box: https://raw.githubusercontent.com/VanirDev/WeaponsDrawn/master/dist/module.json
+3. Click "Install", and once the module has finished installing enable the module in "Manage Modules" in the "Game Settings" tab.
+
+## License
+
+This module is licensed using the Creative Commons Attributions International License, any adaptations must provide both credit and indication of changes made.
+
--- dist/WeaponsDrawn.js
+++ dist/WeaponsDrawn.js
@@ -22,7 +22,7 @@ Hooks.once('init', async function () {
 
 	// Register custom module settings
 	registerSettings();
-	CONFIG.debug.hooks = true; // For debugging only
+	//CONFIG.debug.hooks = true; // For debugging only
 	// Preload Handlebars templates
 	await preloadTemplates();
 
@@ -46,6 +46,16 @@ Hooks.once('ready', function () {
 Hooks.on('renderTokenConfig', onRenderTokenConfig);
 Hooks.on('createCombatant', onCreateCombatant);
 Hooks.on('deleteCombatant', onDeleteCombatant);
+Hooks.on('createToken', onCreateToken);
+
+function onCreateToken (scene, token, _, userId) {
+	if (game.userId !== userId) {
+		// Only act if we initiated the update ourselves
+		return;
+	}
+
+	updateTokenImg(token._id, false, undefined, scene._id);
+}
 
 function onCreateCombatant (combat, combatant, _, userId) {
 	if (game.userId !== userId) {
@@ -95,26 +105,31 @@ function getStateTokenImgPath (actorEntity, inCombat) {
 }
 
 function updateTokenImg (tokenId, inCombat, tokenImgPath, sceneId) {
-	tokenImgPath = tokenImgPath == undefined ? getStateTokenImgPath(game.actors.get(token.actorId), inCombat) : tokenImgPath;
+	tokenImgPath = tokenImgPath == undefined ? getStateTokenImgPath(game.actors.get(game.scenes.get(sceneId).getEmbeddedEntity("Token", tokenId).actorId), inCombat) : tokenImgPath;
 	// console.log(tokenId);
 	// console.log(sceneId);
-	game.scenes.get(sceneId).updateEmbeddedEntity("Token", {_id: tokenId, img:tokenImgPath});
+	if (game.actors.get(game.scenes.get(sceneId).getEmbeddedEntity("Token", tokenId).actorId).getFlag("WeaponsDrawn", "enabled")) {
+		game.scenes.get(sceneId).updateEmbeddedEntity("Token", {_id: tokenId, img:tokenImgPath});
+	}
 }
 
 function onRenderTokenConfig (tokenConfig, html) {
 	const tokenImageDiv = $("input.image").parent().parent();
 	const saveButton = $('button[name="submit"]')
 	let actorEntity = game.actors.get(tokenConfig.actor.data._id);
+	console.log(tokenImageDiv.parent());
 	// console.log(tokenImageDiv);
 	// console.log(saveButton);
 	// console.log(actorEntity);
 
 	let idleTokenImage = "";
 	let combatTokenImage = "";
+	let wdEnabled = false;
 
 	if (actorEntity.data.flags.hasOwnProperty("WeaponsDrawn")) {
 		idleTokenImage = actorEntity.getFlag("WeaponsDrawn", "idle");
 		combatTokenImage = actorEntity.getFlag("WeaponsDrawn", "combat");
+		wdEnabled = actorEntity.getFlag("WeaponsDrawn", "enabled");
 	}
 	if (idleTokenImage == "" || idleTokenImage == undefined) {
 		idleTokenImage = actorEntity.data.img;
@@ -126,17 +141,19 @@ function onRenderTokenConfig (tokenConfig, html) {
 	idleTokenImage = idleTokenImage == "" || idleTokenImage == undefined ? defaultIcon : idleTokenImage;
 	combatTokenImage = combatTokenImage == "" || combatTokenImage == undefined ? defaultIcon : combatTokenImage;
 	
+	$('label:contains("Token Image Path:")').text("Default Token Image:");
 	
 	const headerHTML = 
 		`<div class="form-group">
-			<label>Token Icons</label>
+			<label>Enable Weapons Drawn Tokens:</label>
+			<input id="WDEnableCheckbox" type="checkbox" name="enableWeaponsDrawn" data-dtype="Boolean">
 		</div>`;
 
 	const defaultIconHTML = 
 		`<div class="form-group">
 			<label>Out of Combat Token Image:</label>
 			<div class="form-fields">
-				<button type="button" class="file-picker" data-type="imagevideo" data-target="imgIdle" title="Browse Files" tabindex="-1">
+				<button id="WDIdleTokenButton" type="button" class="file-picker" data-type="imagevideo" data-target="imgIdle" title="Browse Files" tabindex="-1">
 					<i class="fas fa-file-import fa-fw">
 					</i>
 				</button>
@@ -148,7 +165,7 @@ function onRenderTokenConfig (tokenConfig, html) {
 		`<div class="form-group">
 			<label>In Combat Token Image:</label>
 			<div class="form-fields">
-				<button type="button" class="file-picker" data-type="imagevideo" data-target="imgCombat" title="Browse Files" tabindex="-1">
+				<button id="WDCombatTokenButton" type="button" class="file-picker" data-type="imagevideo" data-target="imgCombat" title="Browse Files" tabindex="-1">
 					<i class="fas fa-file-import fa-fw">
 					</i>
 				</button>
@@ -159,24 +176,50 @@ function onRenderTokenConfig (tokenConfig, html) {
 	tokenImageDiv.before(headerHTML);
 	tokenImageDiv.before(defaultIconHTML);
 	tokenImageDiv.before(combatIconHTML);
-	tokenImageDiv.hide();
 
-	tokenImageDiv.prev().prev().click(async (ev) => {
+	if (!wdEnabled) {
+		tokenImageDiv.prev().prev().hide();
+		tokenImageDiv.prev().hide();
+		tokenImageDiv.show()
+	} else {
+		$('input#WDEnableCheckbox').prop('checked', true);
+		tokenImageDiv.hide();
+	}
+
+	$('#WDIdleTokenButton').click(async (ev) => {
 		await new FilePicker({ type:"image", current:idleTokenImage, callback: (path) => {
 			$("#WDIdleTokenPathBox").val(path);
 			//$('input[name="img"]').val(path);
 			idleTokenImage = path;
 		}}).render(true);
 	});
-	tokenImageDiv.prev().click(async (ev) => {
+	$('#WDCombatTokenButton').click(async (ev) => {
 		await new FilePicker({ type:"image", current:combatTokenImage, callback: (path) => {
 			$("#WDCombatTokenPathBox").val(path);
 			combatTokenImage = path;
 		}}).render(true);
 	});
-	saveButton.bind("click", function() {
-		actorEntity.setFlag("WeaponsDrawn", "idle", idleTokenImage);
-		actorEntity.setFlag("WeaponsDrawn", "combat", combatTokenImage);
+	$('input#WDEnableCheckbox').change(function() {
+		wdEnabled = $('input#WDEnableCheckbox').is(":checked");
+		console.log(wdEnabled);
+		if (!wdEnabled) {
+			tokenImageDiv.prev().prev().hide();
+			tokenImageDiv.prev().hide();
+			tokenImageDiv.show();
+		} else {
+			tokenImageDiv.prev().prev().show();
+			tokenImageDiv.prev().show();
+			tokenImageDiv.hide();
+		}
+	});
+	saveButton.bind("click", async function() {
+		await actorEntity.setFlag("WeaponsDrawn", "idle", idleTokenImage);
+		await actorEntity.setFlag("WeaponsDrawn", "combat", combatTokenImage);
+		await actorEntity.setFlag("WeaponsDrawn", "enabled", wdEnabled);
+
+		if (!wdEnabled) {
+			return;
+		}
 
 		let inCombatTokens = [];
 		let idleTokens = [];
@@ -197,6 +240,7 @@ function onRenderTokenConfig (tokenConfig, html) {
 		game.scenes.forEach((scene, sceneKey) => {
 			scene.data.tokens.forEach(token => {
 				if (token.actorId == actorEntity.data._id && !inCombatTokens.includes(token._id)) {
+					console.log(idleTokenImage);
 					updateTokenImg(token._id, false, idleTokenImage, sceneKey);
 					idleTokens.push(token._id);
 				}
